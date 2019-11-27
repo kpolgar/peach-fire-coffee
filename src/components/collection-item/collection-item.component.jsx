c61848e1ffaf7e04568ae2a7701fc086b8e841e0
Kinga Polgar <kpolgar27@gmail.com>
2019-11-26 20:14:58 -0500

    created cart-item component and connect our cart dropdown with cartItems reducer

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
 
+import { connect } from 'react-redux';
+import { addItem } from '../../redux/cart/cart.actions'
+
+import CustomButton from '../custom-button/custom-button.component';
+
 import './collection-item.styles.scss'
 
 import './collection-item.styles.scss';
 
-const CollectionItem = ({ id, name, price, imageUrl }) => (
+const CollectionItem = ({ item, addItem }) => {
+    const { name, price, imageUrl } = item;
+    return (
     <div className = 'collection-item'>
         <div 
             className='image'
@@ -16,7 +23,15 @@ const CollectionItem = ({ id, name, price, imageUrl }) => (
             <span className='name'>{ name }</span>
             <span className='price'>{ price }</span>
         </div>
+        <CustomButton onClick={() => addItem(item)} inverted> Add to Cart </CustomButton>
     </div>
-)
+)}
+//we are creating an addItem function that receives the item as a property, pass it into addItem
+//action creator, which gives us back that object where the prop is equal
+//to add item and the payload is equal to the item that got passed in, and then we will
+//dispath that new object into our store
+const mapDispatchToProps = dispatch => ({
+    addItem: item => dispatch(addItem(item))
+})
 
-export default CollectionItem;
+export default connect(null, mapDispatchToProps)(CollectionItem);
