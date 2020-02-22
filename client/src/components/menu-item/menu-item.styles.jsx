34341089f566dced19253276adb3af30feb51440
Kinga Polgar <kpolgar27@gmail.com>
2020-02-22 11:12:21 -0500

    updating styling of homepage

--- client/src/components/menu-item/menu-item.styles.jsx
+++ client/src/components/menu-item/menu-item.styles.jsx
@@ -1,27 +1,27 @@
 import styled from 'styled-components';
 
 export const MenuItemContainer = styled.div`
-	height: ${({ size }) => (size ? '380px' : '240px')};
-	min-width: 30%;
-	overflow: hidden;
-	flex: 1 1 auto;
-	display: flex;
-	align-items: center;
-	justify-content: center;
-	border: 1px solid black;
-	margin: 0 7.5px 15px;
-	overflow: hidden;
-	&:hover {
-		cursor: pointer;
-		& .background-image {
-			transform: scale(1.1);
-			transition: transform 6s cubic-bezier(0.25, 0.45, 0.45, 0.95);
-		}
-		& .content {
-			opacity: 0.9;
-		}
-	}
-	&:first-child {
+  height: ${({ size }) => (size ? '380px' : '240px')};
+  min-width: 30%;
+  overflow: hidden;
+  flex: 1 1 auto;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 5px;
+  margin: 0 7.5px 15px;
+  overflow: hidden;
+  &:hover {
+    cursor: pointer;
+    & .background-image {
+      transform: scale(1.1);
+      transition: transform 6s cubic-bezier(0.25, 0.45, 0.45, 0.95);
+    }
+    & .content {
+      opacity: 0.9;
+    }
+  }
+  &:first-child {
     margin-right: 7.5px;
   }
   &:last-child {
@@ -37,8 +37,10 @@ export const BackgroundImageContainer = styled.div`
   height: 100%;
   background-size: cover;
   background-position: center;
+  border-radius: 5px;
   background-image: ${({ imageUrl }) => `url(${imageUrl})`};
 `;
+
 
 export const ContentContainer = styled.div`
   height: 90px;
@@ -47,10 +49,10 @@ export const ContentContainer = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  border: 1px solid black;
-  background-color: white;
+  background-color: #ffe7dc;
   opacity: 0.7;
   position: absolute;
+  border-radius: 5px;
 `;
 
 export const ContentTitle = styled.span`
--- client/src/redux/directory/directory.reducer.js
+++ client/src/redux/directory/directory.reducer.js
@@ -2,32 +2,32 @@ const INITIAL_STATE = {
     sections: [
         {
           title: 'hats',
-          imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
+          imageUrl: 'https://images.unsplash.com/photo-1579542145952-05fd27275991?ixlib=rb-1.2.1&auto=format&fit=crop&w=687&q=80',
           id: 1,
           linkUrl: 'shop/hats'
         },
         {
           title: 'jackets',
-          imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
+          imageUrl: 'https://images.unsplash.com/photo-1521053432093-b5d85e9f0946?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1533&q=80',
           id: 2,
           linkUrl: 'shop/jackets'
         },
         {
           title: 'sneakers',
-          imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
+          imageUrl: 'https://images.unsplash.com/photo-1549971352-c31ced98e984?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80',
           id: 3,
           linkUrl: 'shop/sneakers'
         },
         {
           title: 'women\'s',
-          imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
+          imageUrl: 'https://images.unsplash.com/photo-1511242962912-ba18dcf39f30?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80',
           size: 'large',
           id: 4,
           linkUrl: 'shop/womens'
         },
         {
           title: 'men\'s',
-          imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
+          imageUrl: 'https://images.unsplash.com/photo-1553988198-6500949e8dd4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=679&q=80',
           size: 'large',
           id: 5,
           linkUrl: 'shop/mens'
